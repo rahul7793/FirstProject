c6398d9256ad855f8ecb1fb14ac9f6296dda2034
Rahul Kumar Sharma <rahulsharma.7793@gmail.com>
2018-02-22 01:58:02 +0530

    Update FindEvenNoModDiv.java

--- FindEvenNoModDiv.java
+++ FindEvenNoModDiv.java
@@ -21,10 +21,14 @@ public class FindEvenNoModDiv {
 			System.out.println("Enter a num to find Even/Odd: ");
 			Integer num = sc.nextInt();
 			
-			char []ch = num.toString().toCharArray();
-
-			
+			char []ch = num.toString().toCharArray();			
 			System.out.println("its: "+checkOddEven(ch));
+			
+			//another way to do it is using bitwise operator
+			if((num & 1) != 0)
+				System.out.println("odd");
+			else 
+				System.out.println("even");
 		}
 	}
 

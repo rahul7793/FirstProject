75a5e345feec5ca2023458da65149bf4d842ba4b
Rahul Kumar Sharma <rahulsharma.7793@gmail.com>
2018-02-22 01:42:14 +0530

    Add files via upload
    Check even or odd without using  modulus and division operator

---
+++ FindEvenNoModDiv.java
@@ -0,0 +1,31 @@
+package tester;
+
+import java.util.Scanner;
+
+public class FindEvenNoModDiv {
+	
+	static String checkOddEven(char[] ch){
+		for(int i = 0; i < 5; i++){
+			if(Character.getNumericValue(ch[ch.length-1]) == (2*i)){
+//				System.out.println("its Even");
+				return "EVEN";
+			}
+			
+		}
+		return "ODD";
+	}
+
+	public static void main(String[] args) {
+
+		try(Scanner sc = new Scanner(System.in)){
+			System.out.println("Enter a num to find Even/Odd: ");
+			Integer num = sc.nextInt();
+			
+			char []ch = num.toString().toCharArray();
+
+			
+			System.out.println("its: "+checkOddEven(ch));
+		}
+	}
+
+}
